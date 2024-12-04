fe0a096ed57fbf62315a636e690ed479612fea54
pedrohercules0810 <pedrosouzagrangeiro@gmail.com>
2024-12-04 19:54:38 -0300

    model Professor

--- config/users/models.py
+++ config/users/models.py
@@ -11,3 +11,17 @@ class aluno(models.Model):
     class Meta:
         verbose_name = "Aluno"
         verbose_name_plural = "Alunos"
+
+class Professor(models.Model):
+
+    nome = models.CharField(max_length=140)
+    matricula = models.CharField(max_length=12)
+    curso = models.CharField(max_length=140)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Professor"
+        verbose_name_plural = "Professores"
