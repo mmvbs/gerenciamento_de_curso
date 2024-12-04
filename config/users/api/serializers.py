676c5d9b3f494a33453034d299b2602d44f0721c
pedrohercules0810 <pedrosouzagrangeiro@gmail.com>
2024-12-04 20:30:07 -0300

    serializers e views

--- config/users/api/serializers.py
+++ config/users/api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from users.models import aluno
+from users.models import aluno, Professor
 class alunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = aluno
         fields = '__all__'
+
+class ProfessorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Professor
+        fields = "__all__"
+
+class ProfessorCreateSerializer(serializers.Serializer):
+    nome = serializers.CharField(max_length=140)
+    matricula = serializers.CharField(max_length=12)
+    curso = serializers.CharField(max_length=140)
+    login = serializers.CharField(max_length=100)
+    senha = serializers.CharField(max_length=100)
+
     
--- config/users/api/views.py
+++ config/users/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
-from users.models import aluno
-from users.api.serializers import alunoSerializer
+from users.models import aluno, Professor
+from users.api.serializers import alunoSerializer, ProfessorSerializer, ProfessorCreateSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import Group
 
 class alunoViewSet(ModelViewSet):
     serializer_class = alunoSerializer
@@ -21,4 +22,30 @@ class alunoViewSet(ModelViewSet):
         )
 
         serializer_saida = alunoSerializer(novo_aluno)
-        return Response({"Info": "Cadatro realizado!", "data": serializer_saida.data}, status=status.HTTP_201_CREATED)
+        return Response({"Info": "Cadatro do aluno realizado!", "data": serializer_saida.data}, status=status.HTTP_201_CREATED)
+    
+class ProfessorViewSet(ModelViewSet):
+    serializer_class = ProfessorSerializer
+    permission_classes = [AllowAny]
+    queryset = Professor.objects.all()
+
+    def criar_Professor(self, request):
+        serializer = ProfessorCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        novo_user = Professor.objects.create_user(
+            username=serializer.validated_data['login'],
+            password=serializer.validated_data['senha'],
+        )
+        grupo_professores = Group.objects.get(name="Professores")
+        novo_user.groups.add(grupo_professores)
+
+        novo_professor = Professor.objects.create(
+            nome=serializer.validated_data['nome'],
+            matricula=serializer.validated_data['matricula'],
+            curso=serializer.validated_data['curso'],
+            user=novo_user
+        )
+
+        serializer_saida = ProfessorSerializer(novo_professor)
+        return Response({"Info": "Cadastro do professor realizado!", "data":serializer_saida.data}, status=status.HTTP_201_CREATED)
